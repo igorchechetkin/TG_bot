093157f601215bcfebc14251e41b9e9d461e9cca
Igor Chechetkin <0661554596.i.ch@gmail.com>
2023-04-11 23:39:58 +0300

    Add docstrings database/core.py

--- database/core.py
+++ database/core.py
@@ -16,26 +16,51 @@ crud = CRFactory
 
 @db_request("store", db, PlayersHistory, site_api.site_api_handle("players"))
 class DBStorePlayers(CRFactory):
+    """
+    Дочерний класс, записывает данные об игроках в БД. Родитель CRFactory. Принимает данные из запроса к сайту,
+    прогоняет через декоратор CRUDParameters и дальнейшая работа происходит в родительском классе.
+    """
     pass
 
 
 @db_request("store", db, TeamsHistory, site_api.site_api_handle("teams"))
 class DBStoreTeams(CRFactory):
+    """
+    Дочерний класс, записывает данные о командах в БД. Родитель CRFactory. Принимает данные из запроса к сайту,
+    прогоняет через декоратор CRUDParameters и дальнейшая работа происходит в родительском классе.
+    """
     pass
 
 
 @db_request("retrieve", db, PlayersHistory, PlayersHistory.id, PlayersHistory.first_name, PlayersHistory.last_name)
 class DBRetrievePlayers(CRFactory):
+    """
+    Дочерний класс, отдает данные об игроках из БД. Родитель CRFactory. Прогоняет через декоратор CRUDParameters
+    и дальнейшая работа происходит в родительском классе.
+    """
     pass
 
 
 @db_request("retrieve", db, TeamsHistory, TeamsHistory.id, TeamsHistory.conference, TeamsHistory.full_name)
 class DBRetrieveTeams(CRFactory):
+    """
+    Дочерний класс, читает данные о командах из БД. Родитель CRFactory. Прогоняет через декоратор CRUDParameters,
+    и дальнейшая работа происходит в родительском классе.
+    """
     pass
 
 
 class DBFactory():
+    """
+    Класс-фабрика. Организует работу классов, которые взаимодействуют с базой данных.
 
+    Attrs:
+    STORED (dict): словарь объектов записи в БД. Где ключ - команда, значение - объект, осуществляющий
+                   запись в нужную таблицу
+
+    RETRIEVED (dict): словарь объектов чтения из БД. Где ключ - команда, значение - объект, осуществляющий
+                      чтение из нужной таблицы
+    """
     STORED = {
         "db_players": DBStorePlayers,
         "db_teams": DBStoreTeams
@@ -48,12 +73,19 @@ class DBFactory():
 
     @classmethod
     def handle(cls, command: str):
+        """
+        Метод класса-фабрики. Принимает команду в качестве аргумента, далее из атрибутов, по названию команды,
+        достает ссылку на нужный класс для работы с данными. Работа программы продолжается в этих классах.
 
+        :param command (str): команда на получения из атрибутов класса нужной ссылки на объект
+        :return: data
+        """
         cls.STORED.get(command)
 
         data = cls.RETRIEVED.get(command)
 
         if data:
+            print(data)
             return data
 
 
